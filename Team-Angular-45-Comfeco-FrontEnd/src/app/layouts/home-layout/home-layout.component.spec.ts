874996d6fa54cc20f3b1053714876517689e62e1
mausinixterra <diegocali114@hotmail.com>
2021-02-14 22:49:08 -0500

    Cambio de botones

--- Team-Angular-45-Comfeco-FrontEnd/src/app/layouts/home-layout/home-layout.component.spec.ts
+++ Team-Angular-45-Comfeco-FrontEnd/src/app/layouts/home-layout/home-layout.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HomeLayoutComponent } from './home-layout.component';
 
@@ -6,7 +6,7 @@ describe('HomeLayoutComponent', () => {
   let component: HomeLayoutComponent;
   let fixture: ComponentFixture<HomeLayoutComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomeLayoutComponent ]
     })
