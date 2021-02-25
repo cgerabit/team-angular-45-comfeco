b6ad8ea0a9c276f2f3bebd08763157344e3096b2
oswaldonaupa <oswaldo.naupa@tecsup.edu.pe>
2021-02-24 19:54:22 -0500

    Diseño final. Close #73 #75 #77

--- Team-Angular-45-Comfeco-FrontEnd/src/app/layouts/home-layout/home-layout.component.ts
+++ Team-Angular-45-Comfeco-FrontEnd/src/app/layouts/home-layout/home-layout.component.ts
@@ -11,15 +11,29 @@ export class HomeLayoutComponent implements OnInit {
 
   constructor(private router: Router,
     private authService: AuthService) { }
-
+  //variable para abrir el menu
+  sideNavIsOpen: boolean = false;
+  //obtener datos del usuario
   get usuario(){
-    return this.authService.userInfo
+    const { userName } = this.authService.userInfo;
+    return {
+      userName: userName,
+      avatar:
+        `https://avatars.dicebear.com/api/bottts/${userName}.svg`,
+    };
   }
   ngOnInit(): void {
   }
+
   logout(){
     this.router.navigateByUrl('/auth/login');
     this.authService.logout();
+  }
+
+  //abrir el menu al presionar sobre el boton
+  togleResponsiveNavbar(): void {
+    this.sideNavIsOpen = !this.sideNavIsOpen;
+    console.log(this.sideNavIsOpen)
   }
 
 }
--- Team-Angular-45-Comfeco-FrontEnd/src/app/protected/protected.module.ts
+++ Team-Angular-45-Comfeco-FrontEnd/src/app/protected/protected.module.ts
@@ -6,12 +6,28 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 import { SwiperModule } from 'swiper/angular';
 
+//import { SwiperModule } from 'ngx-swiper-wrapper';
+//mport { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+//import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+/*const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};*/
+
+
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
     ProtectedRoutingModule,
     SwiperModule
-  ]
+  ],
+  /*providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ]*/
 })
 export class ProtectedModule { }
