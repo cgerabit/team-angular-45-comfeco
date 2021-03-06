5f33f5cc5de426d3d7aac3e78a6f93a2a9966453
cgerabit <cristian@faucetcollector.com>
2021-03-06 06:06:50 -0400

    Add migration

---
+++ Team-Angular-45-Comfeco-Backend/app/Migrations/20210306100616_social-network-icon.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendComfeco.Migrations
+{
+    public partial class socialnetworkicon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SocialNetworkIcon",
+                table: "SocialNetworks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6a8af04b-0405-4cd2-bc20-d59433235153",
+                column: "ConcurrencyStamp",
+                value: "02e47a45-40c2-40da-8e95-f9962b6410ac");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SocialNetworkIcon",
+                table: "SocialNetworks");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6a8af04b-0405-4cd2-bc20-d59433235153",
+                column: "ConcurrencyStamp",
+                value: "57373875-438b-4719-b708-09c169294d09");
+        }
+    }
+}
